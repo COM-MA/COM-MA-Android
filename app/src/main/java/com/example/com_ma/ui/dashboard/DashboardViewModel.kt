f929d61ec351744c08585071ee047a8a0f5dc3e5
nrj022 <0602smj@gmail.com>
2024-02-10 01:57:59 +0900

    [ADD]#1 안드로이드 초기 파일 추가

---
+++ app/src/main/java/com/example/com_ma/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.com_ma.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
