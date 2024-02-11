c78c38913f4515c5db5a47e9f74b6b59e8b4d2ab
nrj022 <0602smj@gmail.com>
2024-02-12 00:00:25 +0900

    [ADD]#2 Compose 용 Color 파일 생성

---
+++ app/src/main/java/com/example/com_ma/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.com_ma.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Lavender200 = Color(0xFFBCBEFF)
+val Lavender500 = Color(0xFF8D93FF)
+
+val Gray100 = Color(0xFFF3F3F3)
+val Gray300 = Color(0xFFDBDBDB)
+val Gray500 = Color(0xFFAAAAAA)
+val Gray700 = Color(0xFF787878)
+val Gray900 = Color(0xFF3F3F3F)
+
+val Beige500 = Color(0xFFFFEFD3)
+
+val Vermilion500 = Color(0xFFFF4820)
