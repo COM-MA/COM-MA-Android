a0784e127956fe9ed8b79258b4548f2db73964d8
nrj022 <0602smj@gmail.com>
2024-02-14 13:08:03 +0900

    [UI]#7 Card 탭 UI 구현

--- app/src/main/java/com/green/comma/ui/card/CardFragment.kt
+++ app/src/main/java/com/green/comma/ui/card/CardFragment.kt
@@ -5,9 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.green.comma.R
 import com.green.comma.databinding.FragmentCardBinding
+import com.green.comma.ui.WordCardListItem
 
 class CardFragment : Fragment() {
 
@@ -27,6 +38,17 @@ class CardFragment : Fragment() {
 
         _binding = FragmentCardBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.composeViewWordCard.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                LazyVerticalGrid(modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.horizontal_padding)), columns = GridCells.Fixed(2)){
+                    items(10) {index ->
+                        WordCardListItem(painterResource(id = R.drawable.ic_bottom_nav_camera))
+                    }
+                }
+            }
+        }
 
         val textView: TextView = binding.textDashboard
         cardViewModel.text.observe(viewLifecycleOwner) {
--- app/src/main/java/com/green/comma/ui/card/CardViewModel.kt
+++ app/src/main/java/com/green/comma/ui/card/CardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class CardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Card Fragment"
+        value = ""
     }
     val text: LiveData<String> = _text
 }
