705becdf02142a241be040af08408f61e72b9673
nrj022 <0602smj@gmail.com>
2024-02-14 21:36:04 +0900

    [UI]#12 Quiz 페이지 UI 구현

---
+++ app/src/main/java/com/green/comma/ui/quiz/QuizActivity.kt
@@ -0,0 +1,12 @@
+package com.green.comma.ui.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.green.comma.R
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
