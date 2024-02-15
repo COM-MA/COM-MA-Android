f0e5531901f6e18fdfccb807b0c201580c1af798
nrj022 <0602smj@gmail.com>
2024-02-15 12:14:55 +0900

    [FEAT]#13 TTS 구현

---
+++ app/src/main/java/com/green/comma/TTS.kt
@@ -0,0 +1,29 @@
+package com.green.comma
+
+import android.content.Context
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import java.util.Locale
+
+class TTS(val context: Context) {
+    private var textToSpeech: TextToSpeech? = null
+    fun setTTS(){
+        textToSpeech = TextToSpeech(context, TextToSpeech.OnInitListener {
+            if(it == TextToSpeech.SUCCESS){
+                val result = textToSpeech!!.setLanguage(Locale.KOREA)
+                textToSpeech?.setSpeechRate(1.2f)
+                if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                    Log.e("TTS", "해당언어는 지원되지 않습니다.")
+                    return@OnInitListener
+                }
+            }
+        })
+        Log.e("TTS", "TTS 세팅 완료")
+    }
+
+    fun readTTS(word: String){
+        Log.e("TTS", "TTS 출력 시작")
+        textToSpeech?.speak(word, TextToSpeech.QUEUE_FLUSH, null)
+        Log.e("TTS", "TTS 출력 완료")
+    }
+}
