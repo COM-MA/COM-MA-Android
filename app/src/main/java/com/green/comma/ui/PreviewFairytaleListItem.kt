52cf3d95a3f2ac831efef2e4f318345ee54486c3
nrj022 <0602smj@gmail.com>
2024-02-15 17:02:46 +0900

    [UI]#5 동화 프리뷰 리스트 아이템 제작 (Compose)

---
+++ app/src/main/java/com/green/comma/ui/PreviewFairytaleListItem.kt
@@ -0,0 +1,32 @@
+package com.green.comma.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PreviewFairytaleListItem(img: Painter){
+    Surface(
+        modifier = Modifier
+            .width(270.dp)
+            .height(160.dp)
+            .clickable {  }
+            .padding(10.dp),
+        color = Color.White,
+        shape = RoundedCornerShape(12.dp),
+        shadowElevation = 4.dp,
+        onClick = {}
+    ) {
+        Image(contentScale = ContentScale.FillWidth, painter = img, contentDescription = "동화 썸네일")
+    }
+}
