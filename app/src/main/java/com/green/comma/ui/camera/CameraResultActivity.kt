aa22503f41caf1b6afb064b6cc676ef421f513c1
nrj022 <0602smj@gmail.com>
2024-02-15 21:43:00 +0900

    [FEAT]#17 Camera Result 페이지 단어 읽기 (TTS) 구현

--- app/src/main/java/com/green/comma/ui/camera/CameraResultActivity.kt
+++ app/src/main/java/com/green/comma/ui/camera/CameraResultActivity.kt
@@ -3,10 +3,26 @@ package com.green.comma.ui.camera
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.green.comma.R
+import com.green.comma.TTS
+import com.green.comma.databinding.ActivityCameraResultBinding
 
 class CameraResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera_result)
+
+        binding = ActivityCameraResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val tts = TTS(applicationContext)
+        tts.setTTS()
+
+        var text = binding.textWord.text.toString()
+
+        binding.btnSpeaker.setOnClickListener{
+            tts.readTTS(text)
+        }
     }
 }
