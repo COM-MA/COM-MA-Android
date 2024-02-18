e056c5dc5dede39764fbd37e3be9ffaa6cd0b6f9
nrj022 <0602smj@gmail.com>
2024-02-18 23:04:57 +0900

    [FEAT]#20 단어장 조회(최신순) 서버 통신 UI 반영

--- app/src/main/java/com/green/comma/ui/card/CardFragment.kt
+++ app/src/main/java/com/green/comma/ui/card/CardFragment.kt
@@ -2,6 +2,7 @@ package com.green.comma.ui.card
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,11 +10,13 @@ import android.widget.TextView
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.green.comma.R
 import com.green.comma.databinding.FragmentCardBinding
@@ -26,25 +29,39 @@ class CardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val cardViewModel: CardViewModel by viewModels { ViewModelFactory(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val cardViewModel =
-            ViewModelProvider(this).get(CardViewModel::class.java)
+        //val cardViewModel: CardViewModel by viewModels { ViewModelFactory(requireContext()) }
+
+        /*viewModel.viewModelScope.launch {
+            delay(Companion.debounceTime)
+            input?.toString()?.let { viewModel.getUniversityList(it) }
+        }*/
 
         _binding = FragmentCardBinding.inflate(inflater, container, false)
         val root: View = binding.root
         var columCount = 2
 
-        binding.composeViewWordCard.apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                LazyVerticalGrid(modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.horizontal_padding)), columns = GridCells.Fixed(columCount)){
-                    items(10) {index ->
-                        WordCardListItem(painterResource(id = R.drawable.ic_bottom_nav_camera), { moveToCardDetail() })
+        cardViewModel.items.observe(viewLifecycleOwner) { it ->
+            binding.composeViewWordCard.apply {
+                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+                setContent {
+                    LazyVerticalGrid(
+                        modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.horizontal_padding)),
+                        columns = GridCells.Fixed(columCount)
+                    ) {
+                        items(it.size) { item ->
+                            WordCardListItem(it[item].cardImageUrl, { moveToCardDetail() })
+                        }
+                    }
+                    DisposableEffect(Unit) {
+                        cardViewModel.loadLatestCardList()
+                        onDispose {}
                     }
                 }
             }
