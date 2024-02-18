b904fd4f52f0f3641a7b1e9a8691983d0b78de12
nrj022 <0602smj@gmail.com>
2024-02-18 23:37:37 +0900

    [RENAME]#20 단어 리스트 DTO 파일명 변경

--- app/src/main/java/com/green/comma/data/response/card/ResponseCardListDto.kt
+++ app/src/main/java/com/green/comma/data/response/card/ResponseCardListDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ResponseLatestCardListDto(
+data class ResponseCardListDto(
     @SerialName("userCardId")
     val userCardId: Long,
     @SerialName("name")
