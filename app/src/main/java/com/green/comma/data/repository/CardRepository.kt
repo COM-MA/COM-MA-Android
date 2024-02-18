57c28be1304e2d29a9da233b5c87ca157b084569
nrj022 <0602smj@gmail.com>
2024-02-19 02:12:43 +0900

    [CORRECT]#20 변수 명 수정

--- app/src/main/java/com/green/comma/data/repository/CardRepository.kt
+++ app/src/main/java/com/green/comma/data/repository/CardRepository.kt
@@ -4,16 +4,16 @@ import com.green.comma.data.response.card.ResponseCardListDto
 import com.green.comma.data.datasource.CardRemoteDataSource
 
 class CardRepository(
-    private val cardService: CardRemoteDataSource
+    private val cardRemoteDataSource: CardRemoteDataSource
 ) {
     //private val client = ApiClient.getApiClient().create(CardService::class.java)
     /*suspend fun getLatestCardList(): BaseResponse<List<ResponseLatestCardListDto>> {
         return client.getLatestCardList()
     }*/
     suspend fun getLatestCardList(): List<ResponseCardListDto> {
-        return cardService.getLatestCardList().data!!
+        return cardRemoteDataSource.getLatestCardList().data!!
     }
     suspend fun getAlphabetCardList(): List<ResponseCardListDto> {
-        return cardService.getAlphabetCardList().data!!
+        return cardRemoteDataSource.getAlphabetCardList().data!!
     }
 }
