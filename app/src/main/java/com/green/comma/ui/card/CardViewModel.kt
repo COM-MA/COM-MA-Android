1a6f384f13184319ce13e1152d12cd9f7f677ea1
nrj022 <0602smj@gmail.com>
2024-02-19 02:14:42 +0900

    [MOD/DEL]#20 가나다순 로드 함수 변경 / 불필요한 코드 삭제

--- app/src/main/java/com/green/comma/ui/card/CardViewModel.kt
+++ app/src/main/java/com/green/comma/ui/card/CardViewModel.kt
@@ -8,16 +8,10 @@ import com.green.comma.data.response.card.ResponseCardListDto
 import com.green.comma.data.repository.CardRepository
 import kotlinx.coroutines.launch
 
-//class CardViewModel(private val cardRepository: CardRepository) : ViewModel() {
 class CardViewModel(private val cardRepository: CardRepository) : ViewModel() {
 
-    //private val cardRepository = CardRepository(CardRemoteDataSource(ApiClient.getApiClient().create()))
     private val _items = MutableLiveData<List<ResponseCardListDto>>()
     val items: LiveData<List<ResponseCardListDto>> = _items
-    /*private val _items = MutableLiveData<String>().apply {
-        value = "https://img.hankyung.com/photo/201803/03.16142212.1.jpg"
-    }
-    val items: LiveData<String> = _items*/
 
     init {
         loadLatestCardList()
@@ -28,7 +22,6 @@ class CardViewModel(private val cardRepository: CardRepository) : ViewModel() {
     }
     val text: LiveData<String> = _text
     fun loadLatestCardList() {
-        println("최신순 로드")
         viewModelScope.launch {
             val latestCardList = cardRepository.getLatestCardList()
             _items.value = latestCardList
@@ -36,9 +29,8 @@ class CardViewModel(private val cardRepository: CardRepository) : ViewModel() {
     }
 
     fun loadAlphabetCardList() {
-        println("가나다순 로드")
         viewModelScope.launch {
-            val alphabetCardList = cardRepository.getLatestCardList()
+            val alphabetCardList = cardRepository.getAlphabetCardList()
             _items.value = alphabetCardList
         }
     }
