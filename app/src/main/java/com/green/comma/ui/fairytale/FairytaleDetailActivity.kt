03602fed231a0d939b27c4c819710529e950ca74
nrj022 <0602smj@gmail.com>
2024-02-19 15:55:00 +0900

    [UI]#26 동화 상세 페이지 추가 및 UI 구성

---
+++ app/src/main/java/com/green/comma/ui/fairytale/FairytaleDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.green.comma.ui.fairytale
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.green.comma.R
+
+class FairytaleDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fairytale_detail)
+    }
+}
