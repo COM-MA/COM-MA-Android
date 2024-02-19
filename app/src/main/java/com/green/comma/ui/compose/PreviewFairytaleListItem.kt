9ea10cd6414607804c9aff896f770dd35637d728
nrj022 <0602smj@gmail.com>
2024-02-19 12:14:45 +0900

    [UI]#24 Fairytale 프리뷰 아이템 그림자 제거

--- app/src/main/java/com/green/comma/ui/compose/PreviewFairytaleListItem.kt
+++ app/src/main/java/com/green/comma/ui/compose/PreviewFairytaleListItem.kt
@@ -1,16 +1,13 @@
 package com.green.comma.ui.compose
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -24,8 +21,6 @@ fun PreviewFairytaleListItem(imgUrl: String?){
             .clickable {  }
             .padding(10.dp),
         color = Color.White,
-        shape = RoundedCornerShape(12.dp),
-        shadowElevation = 4.dp,
         onClick = {}
     ) {
         AsyncImage(
