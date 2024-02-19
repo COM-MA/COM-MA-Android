b109f9a7c1fce5442ad370749b6f301f7841f9b4
nrj022 <0602smj@gmail.com>
2024-02-19 12:13:28 +0900

    [UI]#23 Card Item 그림자 수정

--- app/src/main/java/com/green/comma/ui/compose/WordCardListItem.kt
+++ app/src/main/java/com/green/comma/ui/compose/WordCardListItem.kt
@@ -1,6 +1,5 @@
 package com.green.comma.ui.compose
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,8 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -24,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.green.comma.data.response.card.ResponseCardListDto
+import com.green.comma.ui.theme.Gray500
 import com.green.comma.ui.theme.Lavender200
 import com.green.comma.ui.theme.Typography
 import com.green.comma.ui.theme.pretendard
@@ -34,10 +34,14 @@ fun WordCardListItem(data: ResponseCardListDto, onClick: () -> Unit, modifier: M
         modifier = modifier
             .width(162.dp)
             .height(195.dp)
-            .padding(10.dp),
+            .padding(10.dp)
+            .shadow(
+                shape = RoundedCornerShape(12.dp),
+                elevation = 6.dp,
+                ambientColor = Gray500,
+                spotColor = Gray500
+            ),
         color = Color.White,
-        shape = RoundedCornerShape(12.dp),
-        shadowElevation = 4.dp,
         onClick = onClick
     ) {
         Column(
