180d05dab575a6b21b1fb4235584bed19db86ea6
nrj022 <0602smj@gmail.com>
2024-02-19 14:18:37 +0900

    [FEAT]#25 Card Detail 서버 통신 구현

---
+++ app/src/main/java/com/green/comma/data/response/card/ResponseCardDetailDto.kt
@@ -0,0 +1,16 @@
+package com.green.comma.data.response.card
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseCardDetailDto(
+    @SerialName("id")
+    val id: Long,
+    @SerialName("name")
+    val name: String,
+    @SerialName("cardImageUrl")
+    val cardImageUrl: String,
+    @SerialName("signImageUrl")
+    val signImageUrl: String
+)
