cf9757fe5d4b0c72c490ec8817e6908f506c664a
nrj022 <0602smj@gmail.com>
2024-02-19 14:16:39 +0900

    [FEAT]#25 Glide로 이미지 불러오기 구현

--- app/src/main/java/com/green/comma/ui/card/CardDetailActivity.kt
+++ app/src/main/java/com/green/comma/ui/card/CardDetailActivity.kt
@@ -2,6 +2,9 @@ package com.green.comma.ui.card
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.green.comma.R
 import com.green.comma.util.Tts
 import com.green.comma.databinding.ActivityCardDetailBinding
@@ -21,13 +24,20 @@ class CardDetailActivity : AppCompatActivity() {
         val tts = Tts(applicationContext)
         tts.setTTS()
 
-        /*binding.composeViewWordView.apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                WordCardView({ tts.readTTS(wordText) }, wordText)
-            }
-        }*/
+        cardViewModel.cardDetailItem.observe(this, Observer { item ->
+            setCard(item)
+        })
+
+        binding.btnCardSpeaker.setOnClickListener {
+            tts.readTTS(binding.tvCardName.text.toString())
+        }
+        setContentView(binding.root)
     }
 
-
+    private fun setCard(item: ResponseCardDetailDto){
+        Glide.with(this)
+            .load(item.cardImageUrl)
+            .into(binding.imgVCardImage)
+        binding.tvCardName.text = item.name
+    }
 }
