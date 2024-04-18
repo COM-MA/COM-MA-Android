f6e905d04bda92dd7ce7416cd8c486e68ffc4575
nrj022 <0602smj@gmail.com>
2024-04-18 23:56:43 +0900

    [UI]#46 별명 설정 페이지 UI 구현

---
+++ app/src/main/java/com/green/comma/ui/auth/AuthNicknameActivity.kt
@@ -0,0 +1,12 @@
+package com.green.comma.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.green.comma.R
+
+class AuthNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth_nickname)
+    }
+}
